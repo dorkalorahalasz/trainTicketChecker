59015184dcae1ccffd9b155c0018a07ce5029532
Dorka Domoszlai-Halász <halaszdodo13@gmail.com>
2023-10-28 22:19:23 +0200

    project basics

---
+++ locomotionmonitor/src/main/java/com/dorkalorahalasz/locomotionmonitor/Main.java
@@ -0,0 +1,14 @@
+package com.dorkalorahalasz.locomotionmonitor;
+
+public class Main {
+    public static void main(String[] args) {
+        //create custom WebDriver
+        WebDriver driver = null;
+
+        //navigate to the target page
+
+        //do the search
+
+        //report the results
+    }
+}
